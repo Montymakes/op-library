3661844577e51d91982deee339b9e014d6084a51
Monty <montydmontilla@gmail.com>
2025-03-31 22:33:09 -0400

    Add delete button to book cards.
    Fix patterns for text inputs.
    
    Style delete and read buttons.

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ const form = document.querySelector('form');
 
 
 //Library Variables and Functions
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -33,15 +33,31 @@ function addBooktoLibrary(book) {
     myLibrary.push(book);
 };
 
+function removeBookfromLibrary(bookID) {
+    const newLibrary = myLibrary.filter((book) => book.id != bookID);
+    myLibrary = newLibrary;
+}
+
+function deleteBook(bookID) {
+    removeBookfromLibrary(bookID);
+    displayLibraryBooks();
+}
+
 function displayLibraryBooks() {
     libraryDisplay.innerHTML = ''; //clear display
 
     for (const book of myLibrary) {
        let bookCard = 
        `<div class="bookCard">
-            <h2 class="title">${book.title}</h2>
-            <p>by ${book.author}</p>
-            <p><span class=bold>Pages:</span> ${book.pages} <span class="bold">Reading Status:</span> ${book.read ? "Finished" : "Not Finished"}</p>
+            <div class="book-details">
+                <h2 class="title">${book.title}</h2>
+                <p>by ${book.author}</p>
+                <p><span class=bold>Pages:</span> ${book.pages}</p>
+            </div>
+            <div class="book-controls">
+                <button type="button" class="${book.read ? "read" : "unread"} readingStatusButton">${book.read ? "READ" : "UNREAD"}</button>
+                <input type="image" src="img/delete.svg" class="delete" alt="Remove this book from your library." id="${book.id}" />
+            </div>
        </div>`;
 
        libraryDisplay.innerHTML += bookCard;
@@ -64,19 +80,19 @@ libraryDisplay.addEventListener("click", (e) => {
     dialog.showModal();
 });
 
+libraryDisplay.addEventListener("click", (e) => {
+    if(!(e.target.closest(".delete"))) return;
+    deleteBook(e.target.id);
+});
+
 exitButton.addEventListener("click", () => {
     clearInputs();
     dialog.close();
 });
 
-function isValidBook(title, author, pages, read) {
-    
-};
 
 function submitNewBook(e) {
     e.preventDefault();
-
-    
 
     const title = titleInput.value;
     const author = authorInput.value;
